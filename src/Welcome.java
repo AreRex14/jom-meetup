068e979f1ed8b19ac270de3f1b01fc657992f0f9
arerex14 <arerifxynwa@gmail.com>
2018-08-23 03:20:45 +0800

    just change name from MainFrame to Welcome

---
+++ src/Welcome.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Welcome extends JFrame implements ActionListener{
+	JButton terus;
+	
+	public Welcome() {
+		Container pane = getContentPane();
+		
+		setLayout(new FlowLayout());
+		setSize(500, 500);
+		setLocation(500, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		//insert app logo here
+		pane.add(new JLabel("Jom Meetup"));
+		pane.add(new JLabel("Di sini permulaan perkongsian ilmu..."));
+		
+		terus = new JButton("Seterusnya");
+		pane.add(terus);
+		terus.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	// for this window testing only
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Welcome();
+	}
+}
