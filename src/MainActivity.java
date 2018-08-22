0bdafa261982cd379690580c52ff0a678cf6d8e5
arerex14 <arerifxynwa@gmail.com>
2018-08-23 03:22:36 +0800

    MainFrame to Welcome, that's all

---
+++ src/MainActivity.java
@@ -0,0 +1,18 @@
+
+public class MainActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// need to create an instance to access attributes or methods
+		new Welcome();
+		// new SignUp();
+		// new VerifySignUp();
+		// new SignIn();
+		// new AppActivity();
+		// new AppActivity2();
+		// new AppActivity3();
+		// new AppActivity4();
+		// new AppActivity5();
+	}
+
+}
