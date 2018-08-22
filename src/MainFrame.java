f36a83f59327ea7018008a7ee11aea1b830c748e
arerex14 <arerifxynwa@gmail.com>
2018-08-23 03:21:17 +0800

    Create MainFrame class as abstract class

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,37 +1,4 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class MainFrame extends JFrame implements ActionListener{
-	JButton terus;
-	
-	public MainFrame() {
-		Container pane = getContentPane();
-		
-		setLayout(new FlowLayout());
-		setSize(500, 500);
-		setLocation(500, 100);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		
-		//insert app logo here
-		pane.add(new JLabel("Jom Meetup"));
-		pane.add(new JLabel("Di sini permulaan perkongsian ilmu..."));
-		
-		terus = new JButton("Seterusnya");
-		pane.add(terus);
-		terus.addActionListener(this);
-	}
+public abstract class MainFrame {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new MainFrame();
-	}
 }
