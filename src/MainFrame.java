c2476b37432a513c2f02e0fcdfe414eb5219d151
arerex14 <arerifxynwa@gmail.com>
2018-08-25 17:33:49 +0800

    so little changes

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,12 @@
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
 
 public abstract class MainFrame extends JFrame{
-
+	public MainFrame() {
+		setLayout(new FlowLayout());
+		setSize(500, 500);
+		setLocation(500, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
