d387df6205a6cd72d3b518cb04841d8294be729e
arerex14 <arerifxynwa@gmail.com>
2018-08-25 17:31:58 +0800

    move settings and search components to superclass

--- src/App.java
+++ src/App.java
@@ -1,4 +1,24 @@
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public abstract class App extends MainFrame {
-
+	JButton tetapan, cari;
+	JTextField carian;
+	
+	// private String searchInput; implement if still has time
+	
+	public App() {
+		Container pane = getContentPane();
+		
+		tetapan = new JButton("Tetapan");
+		pane.add(tetapan);
+		// tetapan.addActionListener(arg0);
+		carian = new JTextField("Cari...");
+		pane.add(carian);
+		cari = new JButton("Cari");
+		pane.add(cari);
+		// cari.addActionListener(arg0);
+	}
 }
